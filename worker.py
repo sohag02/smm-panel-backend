3b20ef6b59654b54ef3a6da5e463fadf27ea374b
Sohag Jabed <sohagjabed10@gmail.com>
2024-11-06 11:37:15 +0530

    retry mechanism for connection

--- worker.py
+++ worker.py
@@ -17,13 +17,18 @@ if __name__ == '__main__':
     logger.info("Starting worker...")
     logger.info(f"Connected to Redis: {redis_conn.ping()}")
     logger.info(f"Listening to queue: {task_queue.name}")
-    
-    worker = Worker(
-        [task_queue],
-        connection=redis_conn,
-        # worker_timezone=pytz.timezone(TIMEZONE)
-        # is_async=False
-    )
-    
-    logger.info("Worker started, waiting for tasks...")
-    worker.work()
+
+    while True:
+        try:
+            logger.info("Connecting to Redis...")
+            worker = Worker(
+                [task_queue],
+                connection=redis_conn,
+                # worker_timezone=pytz.timezone(TIMEZONE)
+                # is_async=False
+            )
+            logger.info("Worker started, waiting for tasks...")
+            worker.work()
+        except Exception:
+            logger.error("Error connecting to Redis")
+
